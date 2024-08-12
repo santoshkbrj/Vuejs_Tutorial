cf05d9bfbb4a47b383747dcfcf999a247fca21d1
santoshkbrj <santoshsah017@gmal.com>
2024-08-12 19:52:37 +0530

    It will throw an error because the local machine doesn't serve the cors. to do so we need to install node js in the same in enviroment.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const count = ref(0);
+        return {
+            count
+        }
+    },
+    template: `<div>Count is {{count}} </div>`
+}
