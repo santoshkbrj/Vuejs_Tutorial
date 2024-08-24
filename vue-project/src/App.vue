c253fd837693ce84e16879cb80634d95fbe42c47
santoshkbrj <santoshsah017@gmal.com>
2024-08-24 17:48:59 +0530

    ref methos API

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Counter from './components/Counter.vue';
 import TemplateSyntax from './components/TemplateSyntax.vue';
+import RefMethod from './components/RefMethod.vue';
 </script>
 
 <template>
   <h1>This is app component</h1>
    <Counter/>
   <template-syntax/>
+  <ref-method/>
 </template>
 
 <style scoped></style>
