f846c3cdbb9560ee60ee80dc65c20f2590a29947
santoshkbrj <santoshsah017@gmal.com>
2024-08-24 18:27:26 +0530

    Computed method return readonly data if required to write we can use get and set method.

--- vue-project/src/components/ComputedComponent.vue
+++ vue-project/src/components/ComputedComponent.vue
@@ -2,15 +2,28 @@
     <div>
         <h3>Computed component</h3>
         <div>Employees present:{{ isEmployeesPresent }}</div>
+        <div>{{ fullName }}</div>
     </div>
 </template>
 <script setup>
-import { computed, reactive } from 'vue';
+import { ref, computed, reactive } from 'vue';
 
     const employees = reactive(['emp1', 'emp2']);
     const isEmployeesPresent = computed(()=>{
      return employees.length>0 ? 'Yes' : 'No'   
-     })
+     });
+
+     const firstName = ref('Santosh');
+     const lastName = ref("Sah");
+     const fullName = computed({
+       get(){
+             return firstName.value + ' ' + lastName.value;
+       },
+       set(newValue){
+         [firstName.value, lastName.value] = newValue.split(' ');
+       }
+     });
+     fullName.value = 'Hi Santosh'
 
 </script>
 <style>
