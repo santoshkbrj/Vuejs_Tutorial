340ef0cf9e9b5e9b237751abd922564e54dddc2c
santoshkbrj <santoshsah017@gmal.com>
2024-08-24 17:31:35 +0530

    Template synctax implementation

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Counter from './components/Counter.vue';
+import TemplateSyntax from './components/TemplateSyntax.vue';
 </script>
 
 <template>
   <h1>This is app component</h1>
    <Counter/>
+  <template-syntax/>
 </template>
 
 <style scoped></style>
---
+++ vue-project/src/components/TemplateSyntax.vue
@@ -0,0 +1,32 @@
+<template lang="">
+    <div>
+        <h3 :[titleAttr]="titleValue">Text Interpolation</h3>
+        <div v-html="message"></div>
+        <div :id="dynamicId">Template Code</div>
+        <div v-bind="objectofAttrs">Object attribute binding</div>
+        <div>Count is 2+2 {{2 + 2}}</div>
+        <div>{{message ? 'Yes': 'No'}}</div>
+        <div>{{Math.floor(Math.random()*8)}}</div>
+        <div v-on:[eventName]="changeTitle">Hello, Good morning</div>
+    </div>
+</template>
+<script setup>
+    const message = '<p>This is simple message</p>';
+    const dynamicId = 'my-id';
+    const objectofAttrs = {
+        id: "my-id",
+        class: 'container',
+        style: 'backgorund-color: red'
+    };
+
+    const titleAttr = 'title';
+    const titleValue = 'title-item';
+    const changeTitle = ()=>{
+      alert('Hello, Good evening');
+    }
+    const eventName = 'click';
+
+</script>
+<style lang="">
+    
+</style>
