0d3350cc59c0159a7f6a157eef5146cbb57b0c52
santoshkbrj <santoshsah017@gmal.com>
2024-08-31 14:44:26 +0530

    passing object data as props

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
-import { ref } from "vue";
-import SinglePostComponent from "./components/SinglePostComponent.vue";
-
-const postTitle = ref('My journey with vue');
-
-const postDetails ={
-  title: ' My Post details ',
-  id: 43
+import SinglePostComponent from './components/SinglePostComponent.vue';
+const post = {
+  id: 1,
+  title: "My Jorney with vue",
 }
+
 </script>
 
 <template>
   <div>
     <h1>This is app component</h1>
-    <single-post-component title="Santosh Web dev" :postTitle="postTitle" :id="43"/>
-    <single-post-component :title="postDetails.title" :id="postDetails.id"/>
+    <single-post-component v-bind="post"/>
   </div>
 </template>
 
--- vue-project/src/components/SinglePostComponent.vue
+++ vue-project/src/components/SinglePostComponent.vue
@@ -1,26 +1,15 @@
 <template>
   <div>
     <h3>Single Post component</h3>
-    <p>{{ id }}-{{ title }}</p>
-    <p>{{ name }}</p>
-    <p>{{ quote }}</p>
+     <p>{{ id }} and {{ title }}</p>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-
- const props = defineProps({
-    title: String,
-    id: Number
- })
- console.log(props.title)
-
- const name = computed(()=> props.title.trim().toLowerCase());
-
- const quote = ref(props.title);
- quote.value = " Life is beautiful ";
- console.log(name.value)
+defineProps({
+  title: String,
+  id: Number
+})
 </script>
 
 <style>
