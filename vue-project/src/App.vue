0bef391d8f036af083013cef50d089c4b2986623
santoshkbrj <santoshsah017@gmal.com>
2024-08-31 15:12:17 +0530

    Passing the event from child to parent using $emit and recevied event as attribute and call the event using @ symbol before it.

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,22 +1,17 @@
 <script setup>
 import { ref } from 'vue';
-import SinglePostComponent from './components/SinglePostComponent.vue';
-
-const post = {
-  id: 1,
-  title: "My Jorney with vue",
+import MyComponent from './components/MyComponent.vue';
+const count = ref(0);
+function increaseCount(n) {
+  count.value += n
 }
-
-const postComment = ref({
-  commentsId:[234, 345, 453, 231],
-})
-
 </script>
 
 <template>
   <div>
     <h1>This is app component</h1>
-    <single-post-component v-bind="post" :commentId="postComment.commentsId"/>
+    {{ count }}
+    <my-component @increase-by="increaseCount"/>
   </div>
 </template>
 
---
+++ vue-project/src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>My component</h3>
+    <button @click="$emit('increaseBy', 1)">
+  Increase by 1
+</button>
+  </div>
+</template>
+
+<script setup>
+ 
+</script>
+
+<style>
+
+</style>
